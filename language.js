414df4dd0aa5bdfd4c09161d991617ee39d593d1
AnthonyInGithub <97834700+AnthonyInGithub@users.noreply.github.com>
2022-01-16 21:12:29 +0800

    Add files via upload

---
+++ language.js
@@ -0,0 +1,70 @@
+var state1 = true;
+var state2 = true;
+var state3 = true;
+function translate_main() {
+	if(state1){
+		//document.write("a");
+		document.getElementById("paragraph1").innerHTML = "1991年12月25日，象征着苏联革命抗争精神的红旗最后一次在克里姆林宫降下，随后被俄罗斯国旗取代。它标志着冷战和苏联时代的结束。此外，它还向我们证实了，完全的共产主义不是一种可行的意识形态。讽刺的是，在苏联解体前夕，一项针对苏共内部观点的大规模调查显示，苏共\'前途光明\'，因为其成员已经适应了\'道德自由、公民社会和市场经济的普遍文明价值观\'。";
+		document.getElementById("paragraph2").innerHTML = "从1988年起，苏联逐渐失去了对莫斯科、波罗的海共和国、高加索和中亚共和国等地区的控制。在1990年的爱沙尼亚最高苏维埃选举中，苏联决定放弃对政治的垄断, 也就意味着共产党将不会永远做为执政党, 而是与其他政党一起进行选举。在随后的选举中，苏联最终在其15个共和国中失去了6个共和国的统治地位(执政党)。";
+		document.getElementById("paragraph3").innerHTML = "1991年8月24日，戈尔巴乔夫辞去苏共中央总书记职务, 并解散了政府的所有其他党派。五天后，最高苏维埃停止了苏共在苏联领土上的所有活动，而此结束了共产主义在苏联的统治。";
+		document.getElementById("paragraph4").innerHTML = "在1991年最后几个月，苏联以惊人的速度解体着。在剩下的10个宪法共和国中，乌克兰首先决定退出。而到9月底，戈尔巴乔夫再也没有能力去影响莫斯科以外的事务了。并且从1991年11月7日起，大多数报纸开始称这个国家为“前苏联”。";
+		document.getElementById("paragraph5").innerHTML = "1991年12月1日，苏联第二大国家乌克兰正式举行全民公决。公投结果显示, 91%的选民倾向于独立，而这意味着乌克兰将正式脱离苏联。尽管戈尔巴乔夫做出了种种努力来挽回，但苏联解体似乎已经是不可改变的事实了。在1991年12月25日晚上，在全国电视讲话中, 戈尔巴乔夫辞去了苏联总统的职务并宣布, 剩余的苏联政府将被俄罗斯政府所取缔。";
+		document.getElementById("paragraph6").innerHTML = "12月25日晚7点32分，戈尔巴乔夫离开克里姆林宫后。苏联国旗降下，苏联国歌最后一次被奏响, 一个时代也即将结束。随后, 俄罗斯国旗升起。";
+		document.getElementById("paragraph7").innerHTML = "苏联解体后的第二天，叶利钦(前俄罗斯总统)搬进了戈尔巴乔夫以前的办公室。而苏联解体后分裂的各个共和国将决定其中央政府在它们国家中的角色和前进方向。";
+		state1 = false;
+	}else{
+		document.getElementById("paragraph1").innerHTML = "On December 25, 1991, the Soviet hammer and sickle flag lowered for the last time over the Kremlin, thereafter replaced by the Russian flag. It signals the end of an era of the Cold War and the USSR. Also, it exhibited that extreme or complete communism isn't a feasible ideology. Ironically, on the eve of the collapse, a large-scale survey of views within the CPSU(Communist Party of the Soviet Union) argued that the party had a great future since its members had adapted to \'general civilisational values of freedom of conscienc, civil society, and the market economy\'.";
+		document.getElementById("paragraph2").innerHTML = "Since 1988 the USSR gradually lost control of Moscow, Baltic republics, Caucasus, and Central Asian Republics, etc. And in the 1990 Estonian Supreme Soviet election, USSR decided to give up the monopoly of political control. In the following elections, USSR eventually lost its dominance in 6 Republics among its 15 Republics.";
+		document.getElementById("paragraph3").innerHTML = "On August 24, 1991, Gorbachev resigned as the general secretary of the Central Committee of the CPSU(Communist Party of the Soviet Union).and dissolved all other parties in the government. Five days after, the Supreme Soviet suspended all the CPSU activities on Soviet Territory, which effectively ended the communist's rule in USSR.";
+		document.getElementById("paragraph4").innerHTML = "The Soviet Union collapsed with dramatic speed in the last quarter of 1991. Among the rest of the 10 constitutional Republics, Ukraine first decided to seceded. By the end of September, Gorbachev was no longer able to influence the affairs out of Moscow. And in the November 7, 1991, most newspapers referred this country as the 'former Soviet Union'.";
+		document.getElementById("paragraph5").innerHTML = "In 1991 December 1, the second largest country Ukraine in USSR had an popular referendum. The referendum showed that 91% of voters prefered independency which confirmed that Ukraine will officially secede from USSR. The disintegration seems to be unchangeable despite the efforts that Gorbachev had made.In a nationally televised speech in the evening of December 25, 1991, Gorbachev resigned as president of the USSR. And he decleared the extinct of USSR office.";
+		document.getElementById("paragraph6").innerHTML = "On the night of December 25, at 7:32 p.m., after Gorbachev left the Kremlin. The Soviet flag was lowered and the State Anthem of the Soviet Union was played for the last time. The Russian flag raise afterward.";
+		document.getElementById("paragraph7").innerHTML = "The following day, Yeltsin moved into Gorbachev's former office. And the rest of few institutions left ceased operation. And the individual republics that split after the Soviet disintegrated would decide their central government's role.";
+		state1 = true;
+	}
+  
+}
+function translate_cause() {
+	if (state2) {
+		document.getElementById("paragraph1").innerHTML = "苏联的解体有着两种理论来解释——结构主义理论和意图主义理论。意图论支持者声称，苏联的崩溃并非是不可避免的结果，它是某些领导人的政策和决定的后果。与之相反的，结构主义者认为苏联的崩溃是一个根深蒂固的结构性问题的结果，结构主义者将其称之为“终将会爆炸的定时炸弹”。";
+		document.getElementById("paragraph2").innerHTML = "一些意图论者将苏联解体归咎于米哈伊尔·戈尔巴乔夫——苏联的第八任总统，也是最后一位苏联领导人。他们提到，戈尔巴乔夫发表的两项政策——改革和民主只造成了负面影响。政策\'改革\'允许公司部分私人化。然而，它失败了，因为它没有全面实施. 与此同时,这个新政策需要时间来巩固。然而，在新体制发挥作用之前，旧体制就崩溃了，这使得苏联经济陷入泥潭之中。当时的GDP和油价就很好的证明了这一点。而政策\'民主\'则允许为地方共产党引入多候选人，虽然不是多党选举，但这也最终使其他共和国试图去脱离苏联。";
+		document.getElementById("paragraph3").innerHTML = "其他一些“意图派”则认为，苏联精英应该为苏联的崩溃负责，因为他们用提倡民族主义和资本主义的方法来从中获得个人利益。此外, 戈尔巴乔夫还曾承认，在切尔诺贝利核事故中，一些领导人的不作为严重影响了苏联政府的信誉(民众不再坚信苏联政府的指引)。";
+		document.getElementById('paragraph4').innerHTML = "与之相反的，结构主义者把苏联解体归咎于苏联结构的缺陷。他们提出了几个观点，如资本主义的发展，民族问题和苏联的斯大林结构。";
+		document.getElementById('paragraph5').innerHTML = "首先，与资本主义相比，完全共产主义(苏联)的分配模式很落后。他们的工业主要集中在重工业，并且由于日常供应是定期配给的，小城市的人们通常缺乏日用品。虽然他们可以在私人商店买到这些日用品，但价格通常会比国营商店高2到3倍。我们可以经常在苏联笑话中看到这种现象(见参考)。";
+		document.getElementById('paragraph6').innerHTML = "第二，尽管苏联试图同化其他非俄罗斯民族，让他们放弃他们的文化和语言(俄罗斯化)，其他社区仍然由于一些政策而强大了起来，如领导人本地化。最终，这导致了苏联内部其他国家的独立。";
+		document.getElementById('paragraph7').innerHTML = "第三，斯大林在1940年之前建立的结构也促成了崩溃。斯大林结构通常忽略个人利益(除了斯大林自己的利益). 在该结构下, 政府通过一系列措施剥夺个人利益, 比如通过剥夺富农的财富来清洗富农。并且前面提到的以重工业为重点的工业模式也是由斯大林建立的。";
+		document.getElementById("reference").innerHTML = "参考: 1.苏联时期莫斯科卢比扬卡（克格勃总部）大楼外。一个愁眉苦脸的男子一边走路一边自言自语：肥皂没有，电池没有，袜子也没有。。。。。。这时旁边走过来一个看起来象是便衣的人小声对他说：公民同志，您要是再这样诋毁我们伟大的社会主义国家，我就要拿手枪把敲你的脑袋了！那个男子看看便衣警察，继续自言自语：看看，连子弹也没有。。。。。。";
+		state2 = false;
+	}else{
+		document.getElementById("paragraph1").innerHTML = "For the Collapse of the Soviet Union, two categories of theories existed -- Structuralist and Intentionalist. Intentionalists claimed that the Soviet collapse was not inevitable and resulted from the policies and decisions of some leaders. In contrast, the Structuralists view that Soviet Collapse was an outcome of deeply rooted structural issues which they called \"time-bomb\".";
+		document.getElementById("paragraph2").innerHTML = "Some Intentionalists attribute the Collapse to Mikhail Gorbachev - the eighth and last leader of the Soviet Union. They pointed out that the two policies that Gorbachev published - Perestroika and Demokratizatsiya have only adverse effects. The Perestroika allows the company to be partially personalized. However, it failed because it doesn't fully implement while this new policy takes time to develop. However, the old system breaks down before the new system work, which leads USSR's economy into chaos. The GDP and oil price at that moment reflect that. Another policy Demokratizatsiya allows the introduction of multi-candidate though not multiparty—elections for the local Communist Party, which eventually made other republics struggle harder to get rid off the USSR."
+		document.getElementById("paragraph3").innerHTML = "Some other Intentionalsits contended that Soviet elites were responsible for the collapse by promoting both nationalism and capitalism from which they could personally benefit. Also, Gorbachev had admitted that some leaders\' misconducts in the Chernobyl nuclear accident have considerably affected the credit of the USSR government.";
+		document.getElementById('paragraph4').innerHTML = "On the contrary, the structuralists attributed the Collapse to the flaws of the USSR. They had mentioned a few reasons, like the advance of capitalism, ethnic problem, and Stalin's policies.";
+		document.getElementById('paragraph5').innerHTML = "Firstly, compared to capitalism, communism has a poor distribution model. Their industry mainly focuses on heavy industry, so since daily supplies were rationing regularly, people in smaller cities usually lack daily supplies. Although they could buy these supplies in a private store, the price was usually 2 to 3 times higher than state-owned store. This perfectly reflects on the Soviet Jokes1.";
+		document.getElementById('paragraph6').innerHTML = "Also, although USSR tried to assimilate other non-Russian communities to give up their culture and language(Russification), other communities were still strengthened by policies like the indigenization of leaders. Eventually, this aid the independence of other nations inside the USSR.";
+		document.getElementById('paragraph7').innerHTML = "Third, Stalin's structure that was set up before 1940 also contributed to the Collapse. Stalin's structure neglected individual interest(except for Stalin himself) like purged the kulaks by taking off their wealth. And the focus on heavy industry production pattern that have been aforementioned was also set up by Stalin.";
+		document.getElementById("reference").innerHTML = "Reference: 1. A famous Soviet Joke: Outside the Lubyanka (KGB headquarters) building in Soviet Moscow.\"No soap, no batteries, no socks...\" a man said to himself as he walked. Another man who appeared to be in plain clothes came by and whispered to him: Comrade citizen, if you do this again against our great communist country, I'll hit you on the head with the handle of my pistol! The man looked at the plainclothes policeman and continued to soliloquise: Look, there are even no bullets..";
+		state2 = true;
+	}
+}
+function translate_consequence() {
+	if(state3){
+		document.getElementById('paragraph1').innerHTML = "苏联的解体有好的一面，也有坏的一面。虽然它留下了许多未解决的问题，但它也留下了自己的遗产。而其他的一些国家也在这一场“盛宴”中获得了扩张的机会。";
+		document.getElementById('paragraph2').innerHTML = "美国是这次崩溃的最大受益者。苏联解体后，美国成为全球唯一一个占有主导地位的超级大国。苏联的崩溃使得美国政府摆脱了由强大对手施加的任何限制。这让美国政府可以对其他国家进行军事干预而不必担心任何报复。此外，由于美苏冷战的结束，欧盟的经济可以有机会复苏，而欧盟的影响力也可以扩展到苏联曾经控制的地区。";
+		document.getElementById('paragraph3').innerHTML = "而苏联内部的遗产也被均匀分配了。由于俄罗斯继承了其前任苏联四分之三的领土、人口、资源和全部核武库，联合国的理事国席位将属于俄罗斯。俄罗斯和哈萨克斯坦将继续使用(+7)的电话代码，而所有苏联大使馆将则会被俄罗斯大使馆取代。此外，在苏联解体之前，它的足球队刚获得了1992年欧洲足球锦标赛的参赛资格。然而，苏联解体后，由于每个成员国都变成了独立的国家, 所以他们都必须以独立国家的身份参赛，所以国际足联把苏联队的战绩给了俄罗斯队。";
+		document.getElementById('paragraph4').innerHTML = "社会主义的苏联和欧洲其他社会主义国家的崩溃，给了共产主义意识形态以沉重的一击。此外，因为几乎所有国家都接受了自由化和自由市场经济的形式，共产主义的思潮被有力的遏制了。";
+		document.getElementById('paragraph5').innerHTML = "此外，俄罗斯经济继续受到旧结构残余的影响。虽然苏联的计划经济在20世纪50年代以前取得了巨大的成功，但在80年代以后它开始衰落。戈尔巴乔夫试图通过他的改革政策来恢复经济，但这只导致了进一步的经济混乱。最终, 这使得在苏联解体后, 俄罗斯在很长一段时间内不得不在经济上依赖美国. ";
+		state3 = false;
+	}else{
+		document.getElementById('paragraph1').innerHTML = "The collapse of the USSR has its good sides and bad sides. It left many unsolved problems but legacies as well existed. Some other countries or unions also got chances to expand themselves during this \"feast\".";
+		document.getElementById('paragraph2').innerHTML = "American is the first beneficiary of this collapse. After the soviet union collapsed, American became the only dominant superpower on a global scale. Collapsed freed the U.S. government from any constraints imposed by a powerful rival. This allowed the U.S. government to intervene in other countries militarily without fear of any retaliation. Moreover, the EU(European Union)'s economy could prosper due to the end of the Cold War between the U.S. and USSR while the EU could extend its influence into areas that USSR once controlled.";
+		document.getElementById('paragraph3').innerHTML = "The legacies inside the USSR were allocated as well. Since Russia inherited 3/4th of territory, population, resources, and the whole nuclear arsenal of its parent USSR, the membership of the UN(united union) would belong to Russia. The telecommunication code(+7) continues to be used by Russia and Kazakhstan while all Soviet embassies would replace by Russian embassies. Also, before its collapse, the soccer team of the soviet had just qualified for the 1992 UEFA European Football Championship. However, after the collapse, every republic in USSR would have to participate as an independent country, so FIFA allocating the Soviet team's record to Russia.";
+		document.getElementById('paragraph4').innerHTML = "The collapse of the socialist U.S.S.R., as well as the other socialist countries of Europe, gave a serious punch to the ideology of communism among the world. Further, the acceptance of liberalization and liberal marketing economies by almost all the countries gave a further impact on the popularity of communism in the world.";
+		document.getElementById('paragraph5').innerHTML = "Besides, the Russian economy continued to suffer from the remnant of the old structure. Although the soviet's command economy obtain great success before the 1950s, it declined after the 1980s. Gorbachev tries to recover the economy through his policy - Perestroika, but it only led to further economical chaos. This eventually made Russia has to economically relied on America after the collapse for a long period.";
+		state3 = true;
+	}
+  
+}
+function non_setup(){
+	window.alert("Sorry, it hasn't set up yet.");
+}
